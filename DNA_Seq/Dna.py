dec7329adb8480e060587b629e04c2ee0bd51ade
Sunath Abeysekara <abeysekarasunath666@gmail.com>
2024-11-27 12:15:16 +0530

    Updated  with GC content
    Updated with GC content to calculate and identify how much strong the DNA seq is.

--- DNA_Seq/Dna.py
+++ DNA_Seq/Dna.py
@@ -9,7 +9,8 @@ def dnagenerate(limit):
         d=d+dd
     return d
 
-Dnaseq=dnagenerate(10)
+
+Dnaseq=dnagenerate(12)
 print("Generated origianl Seq: ",Dnaseq)
 
 
@@ -17,7 +18,7 @@ print("Generated origianl Seq: ",Dnaseq)
 def dnaMutator(): 
     mutatDna=Dnaseq   
     char_list=list(mutatDna)
-    for i in range(2):
+    for i in range(1):#number of mutation=1
         #random index generated for mutation indexus
         index=random.randrange(len(Dnaseq))
         #dna mutation
@@ -51,3 +52,46 @@ print("Transcripted Dna Seq (mRNA): ",transcripteddna)
 
 
 #rna to amino acid
+def aa_convertion(seq):
+    table = { 
+        'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M', 
+        'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T', 
+        'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K', 
+        'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+        'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+        'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+        'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q', 
+        'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R', 
+        'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 
+        'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 
+        'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 
+        'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 
+        'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 
+        'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 
+        'TAC':'Y', 'TAT':'Y', 'TAA':'_', 'TAG':'_', 
+        'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG':'W', 
+    } 
+    protien=''
+    for x in range(0,len(seq),3):
+        codon=seq[x:x+3]
+        protien+=table[codon]
+    print("Protien Sequence of mRNA:\t",protien)
+
+aa_convertion(Dnaseq)
+
+#gc content
+def gc_content(seq):
+    count=0
+    for x in seq:
+        if x=='G'or x=='C':
+            count+=1
+    print("GC Content is:\t",(count/len(seq))*100,'%')
+
+
+whichtogc=input("which DNA to check for gc content 1(original) or else: ")
+if(whichtogc==1):
+    gc_content(Dnaseq)
+else:
+    gc_content(mutatedDnaseq)
+
+#freq score cal
